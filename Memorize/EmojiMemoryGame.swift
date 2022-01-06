862617759c79d28073163ccc0b3f88d760a96592
Usama Fouad <usamaeltmsah@gmail.com>
2022-01-07 00:18:52 +0200

    Create the infrastructure for the model and viewmodel

--- Memorize/EmojiMemoryGame.swift
+++ Memorize/EmojiMemoryGame.swift
@@ -8,5 +8,17 @@
 import SwiftUI
 
 class EmojiMemoryGame {
+    static let emojis = ["😤", "😅", "🤓", "😰", "🍏", "🍍", "🧇", "🍔", "🍟", "🥨", "🍲", "🍙", "🍰", "🥜", "🍿", "🍶", "🍺", "🏀", "🏹", "🥢"]
     
+    static func createMemoryGame() -> MemoryGame<String> {
+        MemoryGame<String>(numberOfPairsOfCards: 4) { pairIndex in
+            EmojiMemoryGame.emojis[pairIndex]
+        }
+    }
+    
+    private var model: MemoryGame<String> = createMemoryGame()
+    
+    var cards: Array<MemoryGame<String>.Card> {
+        return model.cards
+    }
 }
--- Memorize/MemoryGame.swift
+++ Memorize/MemoryGame.swift
@@ -8,15 +8,25 @@
 import Foundation
 
 struct MemoryGame<CardContent> {
-    var cards: Array<Card>
+    private(set) var cards: Array<Card>
     
     func choose(_ card: Card) {
         
     }
     
+    init(numberOfPairsOfCards: Int, createCardContent: (Int) -> CardContent) {
+        cards = Array<Card>()
+        // Add numberOfPairsOfCards x 2 cards array
+        for pairIndex in 0..<numberOfPairsOfCards {
+            let content: CardContent = createCardContent(pairIndex)
+            cards.append(Card(content: content))
+            cards.append(Card(content: content))
+        }
+    }
+    
     struct Card {
-        var isFaceUP: Bool
-        var isMatched: Bool
+        var isFaceUP: Bool = false
+        var isMatched: Bool = false
         var content: CardContent
     }
 }
