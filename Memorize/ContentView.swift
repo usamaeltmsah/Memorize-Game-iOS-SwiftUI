3cabfe6ff3ee2ef4e8fab3a2e05df877f97b6e9f
Usama Fouad <usamaeltmsah@gmail.com>
2022-01-14 22:12:20 +0200

    Remove the unnecessary lines of code

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -11,90 +11,23 @@ struct ContentView: View {
     // viewModel Object
     @ObservedObject var viewModel: EmojiMemoryGame
     
-    @State var emojis = ["😤", "😅", "🤓", "😰", "🍏", "🍍", "🧇", "🍔", "🍟", "🥨", "🍲", "🍙", "🍰", "🥜", "🍿", "🍶", "🍺", "🏀", "🏹", "🥢"]
-    let vehiclesEmojis = ["🚓", "🚕", "🚔", "🚖", "🚗", "🚘", "🚙", "🏎", "🚁", "🚀", "🚂", "🚃", "🚎", "🚢", "🛺", "🛳", "🛬", "🚜"]
-    let fruitsEmojis = ["🍏", "🍎", "🍐", "🍊", "🍋", "🍌", "🍉", "🍇", "🍓", "🫐", "🍈", "🍒", "🍑", "🥭", "🍍", "🥥", "🥝", "🥑"]
-    let animalsEmojis = ["🐶", "🐱", "🐭", "🐹", "🐰", "🦊", "🐻", "🐼", "🐻‍❄️", "🐨", "🐯", "🦁", "🐮", "🐷", "🐽", "🐸", "🐵", "🙈", "🙉", "🐤"]
-    @State var emojiCount = 11
-    var random: Int {
-        Int.random(in: 4..<fruitsEmojis.count)
-    }
-    
-    @State var selectedButtonInd: Int?
     var body: some View {
-        VStack {
-            Text("Memorize!")
-                .font(.largeTitle)
-            ScrollView {
-                LazyVGrid(columns: [GridItem(.adaptive(minimum: 65))]) {
-                    ForEach(viewModel.cards) { card in
-                        CardView(card: card)
-                            .aspectRatio(2/3, contentMode: .fit)
-                            .onTapGesture {
-                                viewModel.choose(card)
-                            }
-                    }
-                }
-                .foregroundColor(.red)
-            }
-            Spacer(minLength: 20)
-                
-            HStack {
-                VStack(alignment: .center) {
-                    vehicles
-                    Text("Vehicles")
-                        .font(.caption)
-                }
-                Spacer()
-                VStack(alignment: .center) {
-                    fruits
-                    Text("Fruits")
-                        .font(.caption)
-                }
-                Spacer()
-                VStack(alignment: .center) {
-                    animals
-                    Text("Animals")
-                        .font(.caption)
+        ScrollView {
+            LazyVGrid(columns: [GridItem(.adaptive(minimum: 65))]) {
+                ForEach(viewModel.cards) { card in
+                    CardView(card: card)
+                        .aspectRatio(2/3, contentMode: .fit)
+                        .onTapGesture {
+                            viewModel.choose(card)
+                        }
                 }
             }
-            .padding(.horizontal)
-            .font(.largeTitle)
-            .foregroundColor(.blue)
-
+            .foregroundColor(.red)
         }
-        .padding()
-    }
-    
-    var vehicles: some View {
-        Button {
-            emojiCount = random
-            emojis = vehiclesEmojis.shuffled()
-        } label: {
-            Image(systemName: "car")
-        }
-    }
-    
-    var fruits: some View {
-        Button {
-            emojiCount = random
-            emojis = fruitsEmojis.shuffled()
-        } label: {
-            Image(systemName: "applelogo")
-        }
-    }
-    
-    var animals: some View {
-        Button {
-            emojiCount = random
-            emojis = animalsEmojis.shuffled()
-        } label: {
-            Image(systemName: "tortoise")
-        }
-    }
-    
-    func widthThatBestFits(cardCount: Int) -> CGFloat {
-        UIScreen.main.bounds.width / CGFloat(cardCount - 1)
+        Spacer(minLength: 20)
+        .padding(.horizontal)
+        .font(.largeTitle)
+        .foregroundColor(.blue)
     }
 }
 
